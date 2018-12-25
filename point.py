dd521bd818eacfcddade29347244b345f395eb31
Michael Rose <michaelrose.vt@gmail.com>
2018-12-25 22:25:23 +0000

    Fix a bug or two in the point class and make it handle more math operations

--- point.py
+++ point.py
@@ -1,7 +1,14 @@
+import cmath
+from typing import Tuple
+
 class Point:
-	def __init__(self, x, y):
+	def __init__(self, x:float, y:float):
 		self._x = x
 		self._y = y
+	
+	@classmethod
+	def fromTuple(cls, _pt: Tuple[float, float]):
+		return cls(_pt[0],_pt[1])
 	
 	@property
 	def x(self):
@@ -10,18 +17,29 @@ class Point:
 	@property
 	def y(self):
 		return self._y
-		
+	
 	@property
 	def pt(self):
+		return self.asTuple()	
+				
+	def asTuple(self) -> Tuple[float, float]:
 		return (self.x, self.y)
 		
 	def rotate(self, angle):
-		ct = math.cos(angle)
-		st = math.sin(angle)
+		ct = cmath.cos(angle)
+		st = cmath.sin(angle)
 		return Point(ct * self.x - st * self.y, st * self.x + ct * self.y)
 
 	def __add__(self, point):
 		return Point(self.x + point.x, self.y + point.y) 
+		
+	def __sub__(self, point):
+		# Implement the minus operator as an addition plus unary minus.
+		return self.__add__(point.__neg__())
+		
+	def __neg__(self):
+		return Point(-self.x, -self.y)
 
 	def __str__(self):
 		return "(%-0.4f, %-0.4f)" % (self.x, self.y)
+
