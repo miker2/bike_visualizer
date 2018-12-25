fa877658b1b37e921f9417accd33bf8d5d6086d1
Michael Rose <michaelrose.vt@gmail.com>
2018-12-25 18:53:57 +0000

    Removed non-class version of cockpit_setup code. Remapped actions.

--- cockpit_setup.py
+++ cockpit_setup.py
@@ -63,63 +63,6 @@ class CockpitSetupView (ui.View):
 		print(self.cockpit_data.handlebar_offset(90))
 								
 		self.close_ui(sender)
-		
-		
-def load_view():
-  v = ui.load_view('cockpit_setup.pyui')
-  v.present('sheet')
-  return v
-
-def close_ui(sender):
-	#print(sender)
-	sender.superview.close()
-
-def validate_data(sender):
-	if len(sender.text) == 0:
-		return False
-	try:
-		int(sender.text)
-		sender.text_color = 'black'
-		return True
-	except ValueError:
-		# Here is where we need to show a warning to the user!
-		sender.text_color = 'red'
-		return False
-		
-def all_valid(view):
-	# Walk through the view entrys and check that they are valid (if they are a TextField)
-	print(view.subviews)
-	form_valid = True
-	for sv in view.subviews:
-		if isinstance(sv, ui.TextField):
-			print("Found text field '%s'!" % (sv.name))
-			try: 
-				form_valid &= sv.action(sv)
-			except TypeError:
-				pass				
-	return form_valid
-		
-def parse_form(sender):
-	print("Clicking this button will parse all of the form entrys and save them to the database!")
-	# We only want to be able to do this once all of the forms entries 
-	# are valid! Until then, the 'Save' button should be grayed out!
-	main_view = sender.superview;
-	if not all_valid(main_view):
-		console.alert('Please finish filling out the form!','OK')
-		return
-	cockpit_data = CockpitConfig(int(main_view['stem_length'].text),
-		int(main_view['stem_angle'].text), int(main_view['top_cap_stack'].text), 
-		int(main_view['spacer_stack'].text), int(main_view['stem_stack'].text))
-	# Now we need to put this data somewhere. Ideally, the cockpit data is associated with a
-	# BikeGeometry object!
-	print(cockpit_data)
-	print(repr(cockpit_data))
-	print(cockpit_data.__dict__)
-	# Just as a test, this works: cockpit_data.__dict__['stem_angle'] = 7
-	print(cockpit_data.as_dict())
-	print(cockpit_data.handlebar_offset(90))
-							
-	close_ui(sender)
 	
 csv = CockpitSetupView()
 print("closed the view")
